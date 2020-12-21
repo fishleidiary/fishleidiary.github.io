f990a8c8fb0f7949c02d772c9b9ddad463b1ba4c
fishleidiary <fishnoediary@gmail.com>
2020-12-21 14:19:07 +0800

    fishlei

--- js/loading.js
+++ js/loading.js
@@ -3,9 +3,9 @@
 // });
 
 $(document).ready(function() {
-$(".loading").fadeTo(1200, 0.8);
+$(".loading").fadeTo(2000, 0.8);
 
 
-setTimeout(function(){$(".loading").attr({style:"display:none"}); }, 1200);
+setTimeout(function(){$(".loading").attr({style:"display:none"}); }, 2000);
 
   });
